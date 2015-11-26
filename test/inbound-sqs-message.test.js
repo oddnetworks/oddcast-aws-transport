e765207aca288fb826ced9e09e497e5a56ad1ba9
Kristoffer Walker <kris@kixx.name>
2015-11-26 08:51:36 -0500

    Use const instead of let in tests
    	modified:   test/inbound-sqs-message.test.js

--- test/inbound-sqs-message.test.js
+++ test/inbound-sqs-message.test.js
@@ -5,9 +5,8 @@ const BRIXX = require('brixx');
 const MessageMixin = require('../lib/inbound-sqs-message');
 
 TEST.suite(function withSNSNotification(suite) {
-	let subject;
-	let createMessage = BRIXX.factory(MessageMixin);
-	let snsMessage = Object.freeze({
+	const createMessage = BRIXX.factory(MessageMixin);
+	const snsMessage = Object.freeze({
 		Type: 'Notification',
 		MessageId: '8a0ba019-e563-5ab0-835e-eed2c72c12a6',
 		TopicArn: 'arn:aws:sns:us-west-2:654728787792:r-and-d-events-transport',
@@ -26,25 +25,23 @@ TEST.suite(function withSNSNotification(suite) {
 			}
 		})
 	});
-	let MessageId = '67f78cd8-e937-49f8-b768-8317dca7f5bc';
-	let ReceiptHandle = 'AQEB1p0i/F99u6VbFtt7E';
-	let Body = JSON.stringify(snsMessage);
-	let MD5OfBody = '7f57108070101e8c6cfb008c8e0a206a';
-	let Attributes = Object.freeze({
+	const MessageId = '67f78cd8-e937-49f8-b768-8317dca7f5bc';
+	const ReceiptHandle = 'AQEB1p0i/F99u6VbFtt7E';
+	const Body = JSON.stringify(snsMessage);
+	const MD5OfBody = '7f57108070101e8c6cfb008c8e0a206a';
+	const Attributes = Object.freeze({
 		SenderId: 'ZIDAIXLAVTELUXBIEIX46',
 		ApproximateFirstReceiveTimestamp: '1448159995491',
 		ApproximateReceiveCount: '3',
 		SentTimestamp: '1448157199780'
 	});
 
-	suite.before(function () {
-		subject = createMessage({
-			MessageId: MessageId,
-			ReceiptHandle: ReceiptHandle,
-			MD5OfBody: MD5OfBody,
-			Body: Body,
-			Attributes: Attributes
-		});
+	const subject = createMessage({
+		MessageId: MessageId,
+		ReceiptHandle: ReceiptHandle,
+		MD5OfBody: MD5OfBody,
+		Body: Body,
+		Attributes: Attributes
 	});
 
 	suite.test(function (test) {
