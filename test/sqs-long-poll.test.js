85fe5c5ebfd734fe90659ef81708aa3b41110d8e
Kristoffer Walker <kris@kixx.name>
2015-11-29 06:33:49 -0500

    Removes test suite hack from long poll test.
    	deleted:    test/index.js
    	modified:   test/sqs-long-poll.test.js

--- test/sqs-long-poll.test.js
+++ test/sqs-long-poll.test.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const Promise = require('bluebird');
+const test = require('tape');
 const BRIXX = require('brixx');
 const sinon = require('sinon');
-const TEST = require('./');
-const LongPollMixin = require('../lib/sqs-long-poll');
+const SQSLongPoll = require('../lib/sqs-long-poll');
 const MessageMixin = require('../lib/inbound-sqs-message');
-const createLongPoll = BRIXX.factory(LongPollMixin);
+const createLongPoll = BRIXX.factory(SQSLongPoll);
 const queueURL = 'http://some.domain.net/url';
 const ReceiptHandle = 'AQEB1p0i/F99u6VbFtt7Ed48PDjQ8ybr0+xu920';
 const Body = '{"pattern":1,"payload":1}';
 const messageFactory = BRIXX.factory(MessageMixin);
 
-TEST.suite(function runTwiceWithoutError(suite) {
+(function runTwiceWithoutError() {
 	const sqs = Object.freeze({
 		receiveMessage: sinon.spy(function mockReceiveMessage(params, callback) {
 			// Call the callback async like the real thing.
@@ -30,70 +30,64 @@ TEST.suite(function runTwiceWithoutError(suite) {
 	});
 	const messages = [];
 
-	suite.before(function () {
-		return new Promise(function (resolve, reject) {
-			const timeout = setTimeout(function () {
-				reject(new Error('timeout: runTwiceWithoutError'));
-			}, 60);
+	test('before runTwiceWithoutError', function (t) {
+		const timeout = setTimeout(function () {
+			throw new Error('timeout: runTwiceWithoutError');
+		}, 60);
 
-			subject.on('error', errorHandler);
+		subject.on('error', errorHandler);
 
-			subject.on('message:received', function (message) {
-				messages.push(message);
-				if (messages.length >= 2) {
-					clearTimeout(timeout);
-					subject.stop();
-					resolve();
-				}
-			});
-
-			subject.start();
+		subject.on('message:received', function (message) {
+			messages.push(message);
+			if (messages.length >= 2) {
+				clearTimeout(timeout);
+				subject.stop();
+				t.end();
+			}
 		});
-	});
 
-	suite.test(function (test) {
+		subject.start();
+	});
+	test('receiveMessage called twice', function (t) {
+		t.plan(1);
+		t.equal(sqs.receiveMessage.callCount, 2);
+	});
+	test('receiveMessage called with parameters', function (t) {
+		t.plan(6);
 		const args = sqs.receiveMessage.args;
 		const paramCalls = Object.freeze(args.map(function (args) {
 			return args[0];
 		}));
-
-		test('receiveMessage called twice', function (t) {
-			t.plan(1);
-			t.equal(sqs.receiveMessage.callCount, 2);
-		});
-		test('receiveMessage called with parameters', function (t) {
-			t.plan(6);
-			paramCalls.forEach(function (params) {
-				t.equal(params.QueueURL, queueURL);
-				t.equal(params.MaxNumberOfMessages, 1);
-				t.equal(params.WaitTimeSeconds, 20);
-			});
-		});
-		test('receives messages', function (t) {
-			const keys = [
-				'MessageId',
-				'ReceiptHandle',
-				'MD5OfBody',
-				'Body',
-				'Attributes',
-				'pattern',
-				'payload'
-			];
-			t.plan(14);
-			messages.forEach(function (message) {
-				Object.keys(message.toJSON()).forEach(function (key) {
-					t.ok(keys.indexOf(key) >= 0, 'key: ' + key);
-				});
-			});
-		});
-		test('error is not emitted', function (t) {
-			t.plan(1);
-			t.equal(errorHandler.callCount, 0);
+		paramCalls.forEach(function (params) {
+			t.equal(params.QueueURL, queueURL);
+			t.equal(params.MaxNumberOfMessages, 1);
+			t.equal(params.WaitTimeSeconds, 20);
 		});
 	});
-});
+	test('receives messages', function (t) {
+		const keys = [
+			'MessageId',
+			'ReceiptHandle',
+			'MD5OfBody',
+			'Body',
+			'Attributes',
+			'pattern',
+			'payload'
+		];
+		t.plan(14);
+		messages.forEach(function (message) {
+			Object.keys(message.toJSON()).forEach(function (key) {
+				t.ok(keys.indexOf(key) >= 0, 'key: ' + key);
+			});
+		});
+	});
+	test('error is not emitted', function (t) {
+		t.plan(1);
+		t.equal(errorHandler.callCount, 0);
+	});
+})();
 
-TEST.suite(function runTwiceWithError(suite) {
+(function runTwiceWithError() {
 	const error = new Error('runTwiceWithError');
 	const sqs = Object.freeze({
 		receiveMessage: sinon.spy(function mockReceiveMessage(params, callback) {
@@ -110,39 +104,34 @@ TEST.suite(function runTwiceWithError(suite) {
 	});
 	const errors = [];
 
-	suite.before(function () {
-		return new Promise(function (resolve) {
-			setTimeout(function () {
-				resolve();
-			}, 36);
+	test('before runTwiceWithError', function (t) {
+		setTimeout(function () {
+			t.end();
+		}, 36);
 
-			subject.on('message:received', messageReceivedHandler);
+		subject.on('message:received', messageReceivedHandler);
 
-			subject.on('error', function (error) {
-				errors.push(error);
-			});
-
-			subject.start();
+		subject.on('error', function (error) {
+			errors.push(error);
 		});
+
+		subject.start();
 	});
-
-	suite.test(function (test) {
-		test('receiveHandler is never called', function (t) {
-			t.plan(1);
-			t.equal(messageReceivedHandler.callCount, 0);
-		});
-		test('error handler called once', function (t) {
-			t.plan(1);
-			t.equal(errors.length, 1);
-		});
-		test('error handler called with an error', function (t) {
-			t.plan(1);
-			t.equal(errors[0], error, 'throw the error');
-		});
+	test('receiveHandler is never called', function (t) {
+		t.plan(1);
+		t.equal(messageReceivedHandler.callCount, 0);
 	});
-});
+	test('error handler called once', function (t) {
+		t.plan(1);
+		t.equal(errors.length, 1);
+	});
+	test('error handler called with an error', function (t) {
+		t.plan(1);
+		t.equal(errors[0], error, 'throw the error');
+	});
+})();
 
-TEST.suite(function deleteMessageWithoutError(suite) {
+(function deleteMessageWithoutError() {
 	const sqs = Object.freeze({
 		deleteMessage: sinon.spy(function mockDeleteMessage(params, callback) {
 			// Call the callback async like the real thing.
@@ -161,25 +150,23 @@ TEST.suite(function deleteMessageWithoutError(suite) {
 		ReceiptHandle: ReceiptHandle
 	});
 
-	suite.before(function () {
+	test('before deleteMessageWithoutError', function (t) {
 		subject.deleteMessage(message);
+		t.end();
 	});
-
-	suite.test(function (test) {
-		test('sqs.deleteMessage is called once', function (t) {
-			t.plan(1);
-			t.equal(sqs.deleteMessage.callCount, 1);
-		});
-		test('sqs.deleteMessage called with', function (t) {
-			t.plan(2);
-			const params = sqs.deleteMessage.args[0][0];
-			t.equal(params.QueueURL, queueURL);
-			t.equal(params.ReceiptHandle, ReceiptHandle);
-		});
+	test('sqs.deleteMessage is called once', function (t) {
+		t.plan(1);
+		t.equal(sqs.deleteMessage.callCount, 1);
 	});
-});
+	test('sqs.deleteMessage called with', function (t) {
+		t.plan(2);
+		const params = sqs.deleteMessage.args[0][0];
+		t.equal(params.QueueURL, queueURL);
+		t.equal(params.ReceiptHandle, ReceiptHandle);
+	});
+})();
 
-TEST.suite(function deleteMessageWithError(suite) {
+(function deleteMessageWithError() {
 	const error = new Error('deleteMessageWithError');
 	const sqs = Object.freeze({
 		deleteMessage: sinon.spy(function mockDeleteMessage(params, callback) {
@@ -201,20 +188,18 @@ TEST.suite(function deleteMessageWithError(suite) {
 
 	const errorHandler = sinon.spy();
 
-	suite.before(function () {
+	test('before deleteMessageWithError', function (t) {
 		subject.on('error', errorHandler);
 		subject.deleteMessage(message);
+		Promise.delay(24).then(t.end);
 	});
-
-	suite.test(function (test) {
-		test('error handler is called once', function (t) {
-			t.plan(1);
-			t.equal(errorHandler.callCount, 1);
-		});
-		test('error handler called with', function (t) {
-			t.plan(1);
-			const err = errorHandler.args[0][0];
-			t.equal(err, error);
-		});
+	test('error handler is called once', function (t) {
+		t.plan(1);
+		t.equal(errorHandler.callCount, 1, 'callCount');
 	});
-});
+	test('error handler called with', function (t) {
+		t.plan(1);
+		const err = errorHandler.args[0][0];
+		t.equal(err, error, 'error instance');
+	});
+})();
