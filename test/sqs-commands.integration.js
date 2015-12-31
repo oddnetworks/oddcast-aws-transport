ecbfac987c5ad6ae83e0f3c3a09e4849fd775cf0
Kristoffer Walker <kris@kixx.name>
2015-12-30 21:25:47 -0500

    Integration tests with command channel
    	modified:   test/sqs-commands.integration.js

--- test/sqs-commands.integration.js
+++ test/sqs-commands.integration.js
@@ -34,7 +34,7 @@ if (!QUEUE_URL) {
 
 (function commandOriginatedMessage() {
 	const paylaod = {
-		id: '123'
+		id: 'command_success'
 	};
 
 	const readTransport = awsTransport.sqsCommandListener({
@@ -58,6 +58,7 @@ if (!QUEUE_URL) {
 	const writeErrorHandler = sinon.spy();
 
 	const messageHandler = sinon.spy();
+	const messageDeletedHandler = sinon.spy();
 
 	test('before all commandOriginatedMessage', function (t) {
 		readTransport.on('error', readErrorHandler);
@@ -69,15 +70,21 @@ if (!QUEUE_URL) {
 		writeTransport.on('error', function () {
 			t.end();
 		});
+
+		readTransport.on('message:deleted', messageDeletedHandler);
 		readTransport.on('message:deleted', function () {
-			t.end();
+			readTransport.close();
 		});
 
 		receiveChannel.use({role: 'test'}, readTransport);
 		sendChannel.use({role: 'test'}, writeTransport);
 
 		// Setup the receive handler.
-		receiveChannel.receive({role: 'test'}, messageHandler);
+		receiveChannel.receive({role: 'test', cmd: 'command:message'}, messageHandler);
+
+		readTransport.on('close', function () {
+			t.end();
+		});
 
 		// Send the command.
 		sendChannel.send({role: 'test', cmd: 'command:message'}, paylaod);
@@ -91,6 +98,131 @@ if (!QUEUE_URL) {
 	test('write error handler is not called', function (t) {
 		t.plan(1);
 		t.equal(writeErrorHandler.callCount, 0);
+	});
+
+	test('got message paylaod', function (t) {
+		t.plan(2);
+		const args = messageHandler.args[0][0];
+		t.ok(typeof paylaod.id !== 'undefined', 'paylaod is present');
+		t.equal(args.id, paylaod.id);
+	});
+
+	test('message:deleted is called only once', function (t) {
+		t.plan(1);
+		t.equal(messageDeletedHandler.callCount, 1);
+	});
+
+	test('after all commandOriginatedMessage', function (t) {
+		readTransport.close();
+		t.end();
+	});
+})();
+
+(function messageHandlersReject() {
+	const paylaod = {
+		id: 'command_failures'
+	};
+
+	const readTransport = awsTransport.sqsCommandListener({
+		queueUrl: QUEUE_URL,
+		region: REGION,
+		accessKeyId: ACCESS_KEY_ID,
+		secretAccessKey: SECRET_ACCESS_KEY
+	});
+
+	const writeTransport = awsTransport.sqsCommandEmitter({
+		queueUrl: QUEUE_URL,
+		region: REGION,
+		accessKeyId: ACCESS_KEY_ID,
+		secretAccessKey: SECRET_ACCESS_KEY
+	});
+
+	const receiveChannel = oddcast.commandChannel();
+	const sendChannel = oddcast.commandChannel();
+
+	const receiveChannelErrorHandler = sinon.spy();
+	const sendChannelErrorHandler = sinon.spy();
+
+	const readErrorHandler = sinon.spy();
+	const writeErrorHandler = sinon.spy();
+
+	const messageDeletedHandler = sinon.spy();
+
+	const successErrorHandler = sinon.spy(function () {
+		return Promise.resolve(null);
+	});
+
+	const refuseErrorHandler = sinon.spy(function () {
+		receiveChannel.remove({role: 'test'}, refuseErrorHandler);
+		receiveChannel.receive({role: 'test'}, successErrorHandler);
+		return Promise.resolve(false);
+	});
+
+	const rejectErrorHandler = sinon.spy(function () {
+		receiveChannel.remove({role: 'test'}, rejectErrorHandler);
+		receiveChannel.receive({role: 'test'}, refuseErrorHandler);
+		return Promise.reject(Error('reject error'));
+	});
+
+	const throwErrorHandler = sinon.spy(function () {
+		receiveChannel.remove({role: 'test'}, throwErrorHandler);
+		receiveChannel.receive({role: 'test'}, rejectErrorHandler);
+		throw new Error('thrown error');
+	});
+
+	test('before all messageHandlersReject', function (t) {
+		// Setup transport error event tests
+		readTransport.on('error', readErrorHandler);
+		writeTransport.on('error', writeErrorHandler);
+
+		// Setup channel error event tests
+		receiveChannel.on('error', receiveChannelErrorHandler);
+		sendChannel.on('error', sendChannelErrorHandler);
+
+		// End the tests when the message is finally deleted from the queue.
+		readTransport.on('message:deleted', messageDeletedHandler);
+		readTransport.on('message:deleted', function () {
+			readTransport.close();
+		});
+
+		// Bind the transports to the channels.
+		receiveChannel.use({role: 'test'}, readTransport);
+		sendChannel.use({role: 'test'}, writeTransport);
+
+		// Setup the receive handler chain.
+		receiveChannel.receive({role: 'test'}, throwErrorHandler);
+
+		readTransport.on('close', function () {
+			t.end();
+		});
+
+		// Send the command.
+		sendChannel.send({role: 'test', cmd: 'command:message'}, paylaod);
+	});
+
+	test('transport error events are not fired', function (t) {
+		t.plan(2);
+		t.equal(readErrorHandler.callCount, 0);
+		t.equal(writeErrorHandler.callCount, 0);
+	});
+
+	test('send channel error events are not fired', function (t) {
+		t.plan(1);
+		t.equal(sendChannelErrorHandler.callCount, 0);
+	});
+
+	test('receive channel error events are fired', function (t) {
+		t.plan(2);
+		const args = receiveChannelErrorHandler.args.map(function (args) {
+			return args[0];
+		});
+		t.equal(args[0].message, 'thrown error');
+		t.equal(args[1].message, 'reject error');
+	});
+
+	test('message:deleted is called only once', function (t) {
+		t.plan(1);
+		t.equal(messageDeletedHandler.callCount, 1);
 	});
 
 	test('after all commandOriginatedMessage', function (t) {
