33981ae8b102c8c30f25f75e7752f7606d73a8db
Kristoffer Walker <kris@kixx.name>
2016-12-29 06:19:51 -0500

    New library utilities for SNS
     Changes to be committed:
    	new file:   experiments/sns/confirmation-request.json
    	new file:   experiments/sns/message-request.json
    	modified:   experiments/sns/publish.js
    	modified:   experiments/sns/server.js
    	deleted:    experiments/sns/sub.js
    	new file:   lib/index.js
    	new file:   lib/sns/aws-sns-client.js
    	new file:   lib/sns/client.js
    	new file:   lib/sns/server.js
    	modified:   package.json

--- experiments/sns/publish.js
+++ experiments/sns/publish.js
@@ -1,30 +1,54 @@
-var AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
 
-console.error('key: %s', process.env.AWS_ACCESS_KEY_ID);
-console.error('secret: %s', process.env.AWS_SECRET_ACCESS_KEY);
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+const AWS_REGION = process.env.AWS_REGION;
 
-var sns = new AWS.SNS({
+const TOPIC_ARN = process.argv[2];
+
+if (!AWS_ACCESS_KEY_ID) {
+	throw new Error('Missing env var AWS_ACCESS_KEY_ID');
+}
+if (!AWS_SECRET_ACCESS_KEY) {
+	throw new Error('Missing env var AWS_SECRET_ACCESS_KEY');
+}
+if (!AWS_REGION) {
+	throw new Error('Missing env var AWS_REGION');
+}
+if (!TOPIC_ARN) {
+	throw new Error('missing argv for topic ARN');
+}
+
+const sns = new AWS.SNS({
+	accessKeyId: AWS_ACCESS_KEY_ID,
+	secretAccessKey: AWS_SECRET_ACCESS_KEY,
 	apiVersion: '2010-03-31',
-	region: 'us-west-2'
+	region: AWS_REGION
 });
 
-var params = {
-	TopicArn: 'arn:aws:sns:us-west-2:654728787792:r-and-d-events-transport',
-	Message: JSON.stringify({foo: 'bar'}),
+const params = {
+	TopicArn: TOPIC_ARN,
+	Message: JSON.stringify({foo: 'bar', timestamp: Date.now()}),
 	MessageAttributes: {
 		pattern: {
 			DataType: 'String',
-			StringValue: JSON.stringify({role: 'logging'})
+			StringValue: JSON.stringify({role: 'messages'})
 		}
 	}
 };
 
-sns.publish(params, function (err) {
+sns.publish(params, (err, data) => {
 	if (err) {
-		console.error(err.stack || err.message || err);
+		console.log('Response Error:');
+		console.log(err.stack || err.message || err);
+	} else {
+		console.log('Subscription Sent to %s %s', params.TopicArn, params.Endpoint);
+		console.log(JSON.stringify(data, null, 2));
+		// {
+		//   "ResponseMetadata": {
+		//     "RequestId": "b39e9d8e-bf22-5405-b82a-cd5085413d37"
+		//   },
+		//   "MessageId": "319a7f8f-d960-51d7-b69f-2f51e303c8e5"
+		// }
 	}
-	// data
-	// { MessageId: '8a0ba019-e563-5ab0-835e-eed2c72c12a6',
-	//   ResponseMetadata: { RequestId: 'f656981e-f7e0-5b58-9fbf-d6d5d5af234e' }
-	// }
 });
--- experiments/sns/server.js
+++ experiments/sns/server.js
@@ -1,83 +1,92 @@
-var http = require('http');
+'use strict';
 
-var server = http.createServer(function (req, res) {
+const http = require('http');
+const https = require('https');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
 	console.log('%s %s', req.method, req.url);
-	Object.keys(req.headers).forEach(function (key) {
+
+	Object.keys(req.headers).forEach(key => {
 		console.log('%s: %s', key, req.headers[key]);
 	});
 
-	var postBody = '';
+	let postBody = '';
 	req.setEncoding('utf8');
-	req.on('data', function (data) {
+
+	req.on('data', data => {
 		postBody += data;
 	});
-	req.on('end', function () {
-		// Subscription Confirmation
-		// -------------------------
-		// POST /
-		// x-amz-sns-message-type: SubscriptionConfirmation
-		// x-amz-sns-message-id: 484a205d-dbbb-47aa-8c86-2f0851c68448
-		// x-amz-sns-topic-arn: arn:aws:sns:us-west-2:654728787792:r-and-d-events-transport
-		// content-length: 1623
-		// content-type: text/plain; charset=UTF-8
-		// host: 104.131.79.48:8686
-		// connection: Keep-Alive
-		// user-agent: Amazon Simple Notification Service Agent
-		// accept-encoding: gzip,deflate
-		//
-		// { Type: 'SubscriptionConfirmation',
-		//   MessageId: '15ef38e5-f1f5-479d-847a-caf1bee9455f',
-		//   Token: '2336412f3... (length: 256)',
-		//   TopicArn: 'arn:aws:sns:us-west-2:654728787...',
-		//   Message: 'You have chosen to subscribe to the topic arn:aws:s... (length: 180)',
-		//   SubscribeURL: 'https://sns.us-west-2.amazonaws.com/?Action=Co... (length: 395)',
-		//   Timestamp: '2015-11-18T21:35:16.047Z',
-		//   SignatureVersion: '1',
-		//   Signature: 'Udh/afAi8uqADP/d7JgH+Wya/M8hetBTbh6CMuzHt3dwbZe1q... (length: 344)',
-		//   SigningCertURL: 'https://sns.us-west-2.amazonaws.com/SimpleN... (length: 98)'
-		// }
-		//
-		// Notification
-		// ------------
-		// POST /
-		// x-amz-sns-message-type: Notification
-		// x-amz-sns-message-id: be0e65e9-4ce9-5d94-a568-2b20a0c94fad
-		// x-amz-sns-topic-arn: arn:aws:sns:us-west-2:654728787792:r-and-d-events-transport
-		// x-amz-sns-subscription-arn: arn:aws:sns:us-west-2:654728787792:r-and-d-events-transport:c76cdc62-af47-4ee6-85d1-733757602152
-		// content-length: 1048
-		// content-type: text/plain; charset=UTF-8
-		// host: 104.131.79.48:8686
-		// connection: Keep-Alive
-		// user-agent: Amazon Simple Notification Service Agent
-		// accept-encoding: gzip,deflate
-		//
-		// { Type: 'Notification',
-		//   MessageId: '027af643-4d1a-5d24-aecd-b88aa16fa962',
-		//   TopicArn: 'arn:aws:sns:us-west-2:654728787792:r-and-d-events-transport',
-		//   Message: '{"foo":"bar"}',
-		//   Timestamp: '2015-11-21T03:51:17.173Z',
-		//   SignatureVersion: '1',
-		//   Signature: 'Gg2Qp/hLPT2zJfzdkIbqS40JC1jl5crDhyq8P5YW39DlT8V/KE6VH1xHGjDwF4XrGmMj3FqtfaMwp+6z... (length: 344)',
-		//   SigningCertURL: 'https://sns.us-west-2.amazonaws.com/SimpleNotificationService-bb750dd426d95ee939... (length: 98)',
-		//   UnsubscribeURL: 'https://sns.us-west-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:... (length: 168)',
-		//   MessageAttributes: { pattern: { Type: 'String', Value: '{"role":"logging"}' } }
-		// }
-		var data = JSON.parse(postBody);
-		if (data.Type === 'SubscriptionConfirmation') {
-			console.log('Token: %s', data.Token);
-			console.log('SubscribeURL: %s', data.SubscribeURL);
-		}
-	});
 
-	var body = 'hello world!';
-	res.writeHead(200, {
-		'Content-Type': 'text/plain',
-		'Content-Length': Buffer.byteLength(body).toString()
+	req.on('end', () => {
+		let data = null;
+		try {
+			data = JSON.parse(postBody);
+		} catch (err) {
+			console.log(`JSON parsing error: ${err.message}`);
+		}
+
+		process.stdout.write('\n');
+		console.log(JSON.stringify(data, null, 2));
+		process.stdout.write('\n');
+
+		let promise;
+
+		if (req.headers['x-amz-sns-message-type'] === 'SubscriptionConfirmation') {
+			console.log('SubscriptionConfirmation SubscribeURL: %s', data.SubscribeURL);
+			promise = sendSubscriptionConfirmation(data.SubscribeURL);
+		} else if (req.headers['x-amz-sns-message-type'] === 'Notification') {
+			console.log('Notification MessageId: %s', data.MessageId);
+			promise = Promise.resolve(null);
+		} else {
+			console.log(`Unknown message type: "${req.headers['x-amz-sns-message-type']}"`);
+			promise = Promise.resolve(null);
+		}
+
+		promise.then(() => {
+			res.writeHead(200, {
+				'Content-Type': 'text/plain',
+				'Content-Length': '0'
+			});
+
+			res.end();
+		}).catch(err => {
+			console.error('ERROR:');
+			console.error(err.stack || err.message || err);
+
+			res.writeHead(500, {
+				'Content-Type': 'text/plain',
+				'Content-Length': '0'
+			});
+
+			res.end();
+		});
 	});
-	res.end(body);
 });
 
-server.listen(8686, function () {
-	var addr = server.address();
+server.listen(8686, () => {
+	const addr = server.address();
 	console.log('server running on %s:%s', addr.address, addr.port);
 });
+
+function sendSubscriptionConfirmation(confirmationUrl) {
+	const parsedUrl = url.parse(confirmationUrl);
+
+	const params = {
+		method: 'GET',
+		protocol: parsedUrl.protocol,
+		hostname: parsedUrl.hostname,
+		path: parsedUrl.path
+	};
+
+	return new Promise((resolve, reject) => {
+		const req = https.request(params, res => {
+			console.log('Subscription confirmation response received.');
+			resolve(res);
+		});
+
+		req.on('error', reject);
+
+		req.end();
+	});
+}
